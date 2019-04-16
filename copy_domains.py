6e6c5c70f156fc32715403d9fc65aaaf6d7bf7a0
root <root@KBP1-V01-F72954.synapse.com>
2019-04-16 18:48:20 +0300

    validation of domains/categories has been added

--- copy_domains.py
+++ copy_domains.py
@@ -1,6 +1,10 @@
+import os
 import boto3
 import botocore
 import argparse
+import datetime
+import sys
+import json
 
 parser = argparse.ArgumentParser(description='Copy recent version of domains to deploy folder')
 parser.add_argument('-b','--bucket', help='name of the NaaS bucket', required=True)
@@ -22,15 +26,71 @@ except botocore.exceptions.ClientError as e:
 else:
   print ("The bucket %s exists" % args.bucket)
 
+client = boto3.client('s3')
+prefix = 'models'
+domains = []
+result = client.list_objects(Bucket=args.bucket, Prefix=prefix + '/', Delimiter='/')
+for o in result.get('CommonPrefixes'):
+  domains.append((o.get('Prefix').strip(prefix).strip('/')))
 
+print (domains)
 
+#prefix1 = 'models/Fthwp_Demo'
+#result1 = client.list_objects(Bucket=args.bucket, Prefix=prefix1 + '/', Delimiter='/')
+#for o in result1.get('CommonPrefixes'):
+#  print (o.get('Prefix'))
 
-#print (naas_bucket)
+#for key in client.list_objects(Bucket=args.bucket, Prefix='models/Fthwp_Demo/', Delimiter='/')['Contents']:
+#  print(key['Key'])
 
-#response = s3.list_buckets()
+bucket = s3.Bucket(args.bucket)
+recent_version = []
+for domain in domains:
+  config_file = []
+  print ("Handling the domain %s " % domain)
+  for s3_file in bucket.objects.all():
+    if (s3_file.key.find('/'+domain+'/') != -1 and s3_file.key.find('ma_resultset.json') != -1 ):
+      config_file.append(s3_file)
 
-#buckets = [bucket['Name'] for bucket in response['Buckets']]
+  print (config_file)
+  print ('-------------')
 
-#print("Bucket List: %s" % buckets)
+  current_object=config_file[0]
 
+  print(current_object)
+  print ('--------------')
+  for s3_file in config_file:
+    if ( current_object.last_modified < s3_file.last_modified):
+      current_object=s3_file
+
+  print (current_object)
+  recent_version.append(current_object)
+
+for f in recent_version:
+  current_category_files=[]
+  domain_is_valid = True
+  print(f.last_modified, f.key)
+  content_object = s3.Object(args.bucket, f.key)
+  file_content = content_object.get()['Body'].read().decode('utf-8')
+  categories= json.loads(file_content)['models']
+  print (categories)
+  for category in categories:
+    category_path = f.key.rpartition('ma_resultset.json')[0]
+    for key in client.list_objects(Bucket=args.bucket, Prefix=category_path+category+'/', Delimiter='/')['Contents']:
+      current_category_files.append(os.path.basename(key['Key']))
+    
+    print('current category files')
+    print (current_category_files)
+    
+    for valid_file in valid_files:
+      if valid_file not in current_category_files:
+        domain_is_valid = False
+        break
+
+  if domain_is_valid == True:
+    print ('Domain is valid '+ f.key)
+  else:
+    print ('Domain is not valid '+ f.key)
+
+print ('---------')
 
