830f9313bdecaca25313519745006ad72830182f
root <root@KBP1-V01-F72954.synapse.com>
2019-04-17 17:27:12 +0300

    finished copy file part of the script

--- copy_domains.py
+++ copy_domains.py
@@ -16,6 +16,9 @@ valid_files = ['architecture.json', 'encodings.json', 'metrics.json', 'model_wei
 
 s3 = boto3.resource('s3')
 
+'''
+Check if the bucket that is passed as a parameter exists in s3
+'''
 try:
   s3.meta.client.head_bucket(Bucket=args.bucket)
 
@@ -27,9 +30,16 @@ except botocore.exceptions.ClientError as e:
 else:
   print ("The bucket %s exists" % args.bucket)
 
+'''
+Clear the /deploy folder which might contain
+previous versions of domains
+'''
 bucket = s3.Bucket(args.bucket)
 bucket.objects.filter(Prefix="deploy/").delete()
 
+'''
+Get the list of domains that should be handled
+'''
 client = boto3.client('s3')
 prefix = 'models'
 domains = []
@@ -37,16 +47,12 @@ result = client.list_objects(Bucket=args.bucket, Prefix=prefix + '/', Delimiter=
 for o in result.get('CommonPrefixes'):
   domains.append((o.get('Prefix').strip(prefix).strip('/')))
 
+print ('Script will check and copy the domains : ')
 print (domains)
 
-#prefix1 = 'models/Fthwp_Demo'
-#result1 = client.list_objects(Bucket=args.bucket, Prefix=prefix1 + '/', Delimiter='/')
-#for o in result1.get('CommonPrefixes'):
-#  print (o.get('Prefix'))
-
-#for key in client.list_objects(Bucket=args.bucket, Prefix='models/Fthwp_Demo/', Delimiter='/')['Contents']:
-#  print(key['Key'])
-
+'''
+Get the most modern version of the model for each domain
+'''
 bucket = s3.Bucket(args.bucket)
 recent_version = []
 for domain in domains:
@@ -56,57 +62,43 @@ for domain in domains:
     if (s3_file.key.find('/'+domain+'/') != -1 and s3_file.key.find('ma_resultset.json') != -1 ):
       config_file.append(s3_file)
 
-  print (config_file)
-  print ('-------------')
-
   current_object=config_file[0]
 
-  print(current_object)
-  print ('--------------')
   for s3_file in config_file:
     if ( current_object.last_modified < s3_file.last_modified):
       current_object=s3_file
 
-  print (current_object)
   recent_version.append(current_object)
 
+'''
+Validate most modern version of model (please see a set of files above)
+and copy them if the model has all of them in place
+'''
 for f in recent_version:
-  print ('---------------------')
+  print ('--------------------------------------------------------')
   domain_is_valid = True
-  print(f.last_modified, f.key)
+
   content_object = s3.Object(args.bucket, f.key)
   file_content = content_object.get()['Body'].read().decode('utf-8')
   categories= json.loads(file_content)['models']
-  print (categories)
+
   for category in categories:
     current_category_files=[]
     category_path = f.key.rpartition('ma_resultset.json')[0]
     for key in client.list_objects(Bucket=args.bucket, Prefix=category_path+category+'/', Delimiter='/')['Contents']:
       current_category_files.append(os.path.basename(key['Key']))
-    
-    print('current category files')
-    print (current_category_files)
-    
+
     for valid_file in valid_files:
       if valid_file not in current_category_files:
         domain_is_valid = False
         break
 
   if domain_is_valid == True:
-    print ('Domain is valid '+ f.key)
-    print (os.path.dirname(os.path.dirname(f.key)))
-#    copy_source = { 'Bucket': args.bucket, 'Key': f.key }
-#    print (f.key)
-#    print ('deploy/' + os.path.relpath(f.key, os.path.dirname(os.path.dirname(os.path.dirname(f.key)))+'/'))
-#    s3.meta.client.copy(copy_source, args.bucket, 'deploy/' + os.path.basename(os.path.dirname(f.key))+'/'+ os.path.relpath(f.key, os.path.dirname(os.path.dirname(f.key))))
+    print ('Domain is valid '+ os.path.basename(os.path.dirname(os.path.dirname(f.key))))
     objs = bucket.objects.filter(Prefix=os.path.dirname(f.key))
     for obj in objs:
       print ('copy from location : ' + obj.key)
       print ('to location : ' + 'deploy/'+ os.path.basename(os.path.dirname(os.path.dirname(f.key)))  +'/'+ os.path.relpath(obj.key, os.path.dirname(f.key)))
       s3.meta.client.copy({'Bucket': args.bucket, 'Key': obj.key}, args.bucket, 'deploy/'+ os.path.basename(os.path.dirname(os.path.dirname(f.key)))  +'/'+ os.path.relpath(obj.key, os.path.dirname(f.key)))
   else:
-    print ('Domain is not valid '+ f.key)
-
-
-print ('---------')
-
+    print ('ERROR: domain is not valid '+ os.path.basename(os.path.dirname(os.path.dirname(f.key))))
